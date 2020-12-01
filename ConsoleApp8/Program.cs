b38df20b746f8906001efbe507ea55776ce5037b
Adionei Lima Sousa <adionei.lima@gmail.com>
2020-12-01 08:50:55 -0300

    Ate agora foi o inicio do projeto

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp8 {
+    class Program {
+        static void Main(string[] args) {
+            List<string> list = new List<string> {"primeiro", "segundo", "terceirodd"};
+            
+            Console.WriteLine(list[1]);
+            string pos1 = list.Find(x => x.Length == 10);
+            Console.WriteLine(pos1);
+           
+        }
+    }
+}
