91849ad5094a1aa5025e9a44d8311612c4d6dc0a
Adionei Lima Sousa <adionei.lima@gmail.com>
2020-12-01 08:55:10 -0300

    Coloquei uma mensagem de bom dia agora

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -5,10 +5,14 @@ namespace ConsoleApp8 {
     class Program {
         static void Main(string[] args) {
             List<string> list = new List<string> {"primeiro", "segundo", "terceirodd"};
+
+
             
             Console.WriteLine(list[1]);
             string pos1 = list.Find(x => x.Length == 10);
             Console.WriteLine(pos1);
+
+            Console.WriteLine("bom dia");
            
         }
     }
